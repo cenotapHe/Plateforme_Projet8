73f406bdf1e8ef2f39df506732e4d1604837d92c
cenotapHe <bat1ste@hotmail.com>
2018-09-19 17:50:06 +0200

    Travis final with OC course

---
+++ Plateforme_Projet8/settings/travis.py
@@ -0,0 +1,21 @@
+from . import *
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': '',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '',
+
+        'HOST': '',
+
+        'PORT': '',
+
+    },
+
+}
