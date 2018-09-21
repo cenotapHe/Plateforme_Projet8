736dc21d77c0174b0ed1ec576060e9714b2bce2c
cenotapHe <bat1ste@hotmail.com>
2018-09-21 16:18:20 +0200

    separe different env

--- Plateforme_Projet8/settings/__init__.py
+++ Plateforme_Projet8/settings/__init__.py
@@ -25,10 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$qstbkk-=+!p61jny)a+3pq=!p)a*#h%5gqct(-grmc&#q-vg_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['plateforme-p8.herokuapp.com', '127.0.0.1']
 
@@ -141,17 +138,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-if os.environ.get('ENV') == 'PRODUCTION':
-
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
